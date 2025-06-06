e9e6e6c717653e52d0a8047ab48f3bef67bc743c
Mathieu Parisot <mathieu.parisot@antescofo.com>
2025-06-06 17:09:20 +0200

    create project

---
+++ index.js
@@ -0,0 +1,23 @@
+import { VerovioToolkit } from 'verovio/esm';
+import createVerovioModule  from 'verovio/wasm';
+import {readFileSync, writeFileSync}  from 'node:fs';
+
+const verovioModule = await createVerovioModule();
+const verovioToolkit = new VerovioToolkit(verovioModule);
+verovioToolkit.setOptions({
+    xmlIdSeed: 1,
+})
+
+try {
+    const data = readFileSync('./test-verovio-grace.xml', 'utf8');
+    verovioToolkit.loadData(data);
+    const timeMap = verovioToolkit.renderToTimemap({
+        includeMeasures: true,
+        includeRests: true,
+    });
+
+    writeFileSync('./generated-timeMap.txt', JSON.stringify(timeMap,  null, 2));
+    writeFileSync('./generated-svg.svg', verovioToolkit.renderToSVG(1));
+} catch (err) {
+    console.error(err);
+}
